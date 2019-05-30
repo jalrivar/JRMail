5e1e349e269b15af156de436f1588121eab7fba2
Jaime Rivera <alexjr26@hotmail.com>
2019-05-29 19:30:22 -0600

    - DAL listo para integrarse.

---
+++ JRMailSqlScripts.sql
@@ -0,0 +1,64 @@
+﻿USE JRMail;
+        
+SET NOCOUNT ON;
+
+/********************************************************************************************************************************************************************************************/
+--Creación de esquema.
+/********************************************************************************************************************************************************************************************/
+DECLARE @ESQUEMA CHAR(3) = 'JRM';
+IF NOT EXISTS (SELECT 'X' FROM sys.schemas WHERE name = @ESQUEMA)
+BEGIN
+	EXEC('CREATE SCHEMA ' + @ESQUEMA)
+END
+
+/********************************************************************************************************************************************************************************************/
+--Creación de tablas.
+/********************************************************************************************************************************************************************************************/
+DROP TABLE IF EXISTS JRM.MailMessageClassifaction;
+DROP TABLE IF EXISTS JRM.MailClassification;
+
+DROP TABLE IF EXISTS JRM.MailMessage;
+DROP TABLE IF EXISTS JRM.MailBox;
+
+CREATE TABLE JRM.MailBox
+(MailBoxId   INT
+ PRIMARY KEY IDENTITY(1, 1) NOT NULL, 
+ MailBoxName VARCHAR(50)
+);
+
+CREATE TABLE JRM.MailMessage
+(MailMessageId INT
+ PRIMARY KEY IDENTITY(1, 1) NOT NULL, 
+ MailBoxId     INT NOT NULL, 
+ UserName      NVARCHAR(256) NOT NULL, 
+ [To]          VARCHAR(250) NOT NULL, 
+ [CC]          VARCHAR(250) NOT NULL, 
+ [BCC]         VARCHAR(250) NOT NULL, 
+ [From]        VARCHAR(250) NOT NULL, 
+ [Subject]     VARCHAR(250) NOT NULL, 
+ Body          VARCHAR(3999) NOT NULL, 
+ [Date]        DATETIME NOT NULL, 
+ MailStatus    INT NOT NULL, 
+ CONSTRAINT FK_MailBox_MailMessage FOREIGN KEY(MailBoxId) REFERENCES JRM.MailBox(MailBoxId)
+);
+
+CREATE TABLE JRM.MailClassification
+(MailClassificationId   INT
+ PRIMARY KEY IDENTITY(1, 1) NOT NULL, 
+ UserName           NVARCHAR(256) NOT NULL, 
+ MailClassificationName VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE JRM.MailMessageClassifaction
+(MailMessageClassifactionId INT
+ PRIMARY KEY IDENTITY(1, 1) NOT NULL, 
+ MailMessageId              INT NOT NULL, 
+ MailClassificationId       INT NOT NULL, 
+ CONSTRAINT FK_Classification_MailMessageClassification FOREIGN KEY(MailClassificationId) REFERENCES JRM.MailClassification(MailClassificationId), 
+ CONSTRAINT FK_MailMessage_MailMessageClassification FOREIGN KEY(MailMessageId) REFERENCES JRM.MailMessage(MailMessageId)
+);
+/********************************************************************************************************************************************************************************************/
+--Inserts para catalogos
+/********************************************************************************************************************************************************************************************/
+INSERT INTO JRM.MailBox (MailBoxName) VALUES ('Inbox');
+INSERT INTO JRM.MailBox (MailBoxName) VALUES ('Sentbox');
